4da3dc09af29aba34ff5f004f2fb61c563c029ca
VincentCheng24 <oncevincentcheng@gmail.com>
2019-03-31 19:43:30 +0200

    delete unused files

--- test.m
+++ test.m
@@ -1,5 +1,11 @@
-data = load('data.mat');
-result = data.data;
-result(:,3) = -result(:,3);
-result(:, 4:6)= normalize(result, 1, 'range');
-result = sortrows(result, 6, 'descend');
+% data = load('data.mat');
+% result = data.data;
+% result(:,3) = -result(:,3);
+% result(:, 4:6)= normalize(result, 1, 'range');
+% result = sortrows(result, 6, 'descend');
+
+a = [1, 2, 3, 4];
+str = sprintf('%d', a(1))
+for i = 2:size(a, 2)
+str = strcat(str, '->', sprintf('%d', a(i)))
+end
